a254eb2e2a22f710c83b92fcaf44ed6cc3d0a9e7
stiven1505 <83484277+stiven1505@users.noreply.github.com>
2023-07-24 10:45:26 -0500

    Botonera Arreglada

--- js/index.js
+++ js/index.js
@@ -5,11 +5,11 @@ var facebookIcon = document.querySelector('.facebook-icon');
 var gmailIcon = document.querySelector('.gmail-icon');
 var whatsappIcon = document.querySelector('.whatsapp-icon');
 
-var landing = document.querySelector('.landing');
-var instagram = document.querySelector('.instagram');
-var facebook = document.querySelector('.facebook');
-var gmail = document.querySelector('.gmail');
-var whatsapp = document.querySelector('.whatsapp');
+var landing = document.getElementById('switcher');
+var instagram = document.getElementById('switcher2');
+var facebook = document.getElementById('switcher3');
+var gmail = document.getElementById('switcher4');
+var whatsapp = document.getElementById('switcher5');
 
 let anchoPantalla;
 let posicionBtn;
@@ -21,27 +21,32 @@ let viewMovil = 430;
 landing.addEventListener('click', function () {
     setTimeout(() => {
         window.location.href = '../landing.html';
+    landing.checked = true;
     }, 1000)
 });
 facebook.addEventListener('click', function () {
     setTimeout(() => {
         window.location.href = 'https://es-la.facebook.com/prodesing.ltda';
+    facebook.checked = true;
     }, 1000)
 });
 
 gmail.addEventListener('click', function () {
     setTimeout(() => {
         window.location.href = 'https://outlook.office.com/mail/deeplink/compose?mailtouri=mailto%3Aproyectos%40prodesing.co';
+    gmail.checked = true;
     }, 1000)
 });
 whatsapp.addEventListener('click', function () {
     setTimeout(() => {
         window.location.href = 'https://api.whatsapp.com/send/?phone=573178165733&text=Hola%2C+quiero+recibir+más+información+para+mi+proyecto+.&type=phone_number&app_absent=0';
+    whatsapp.checked = true;
     }, 1000)
 });
 instagram.addEventListener('click', function () {
     setTimeout(() => {
         window.location.href = 'https://www.instagram.com/prodesing/';
+    instagram.checked = true;
     }, 1000)
 });
 
